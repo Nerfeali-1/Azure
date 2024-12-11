00ed6effea2408120cb643d30893741ca1c9349b
Nerfeali-1 <nerfeali100172@gmail.com>
2024-12-10 21:32:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 document.getElementById("usuarioForm").addEventListener("submit", function(event) {
   event.preventDefault();
-  const usuario = document.getElementById("name").value;  // Usuario del input
-  const email = document.getElementById("email").value;    // Correo del input
+  const usuario = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
 
-  // Imprimir los valores enviados al backend
-  console.log(`Enviando usuario: ${usuario}, email: ${email}`);
-
-  fetch('http://localhost:3000/auth', {  // Cambiar URL si el servidor está desplegado en otro lugar
+  fetch('http://localhost:3000/auth', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -15,15 +12,13 @@ document.getElementById("usuarioForm").addEventListener("submit", function(event
   })
   .then(response => response.json())
   .then(data => {
-    console.log('Respuesta del servidor:', data);  // Verifica la respuesta del backend
     if (data.message === 'Autenticación exitosa') {
       // Si la autenticación es exitosa, redirigir a otra página
-      window.location.href = 'pagina_exitoso.html';  // Cambia la página de destino si es necesario
+      window.location.href = 'pagina_exitoso.html'; // Cambia por la página de destino
     } else {
       alert('Usuario o correo incorrectos');
     }
   })
   .catch(error => console.error('Error:', error));
 });
-', error));
-});
+
